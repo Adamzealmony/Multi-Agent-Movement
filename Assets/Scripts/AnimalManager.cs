07dec9110edf4615c1372dc648ef686ef888cfc7
YINJIE WU <2721541713@qq.com>
2018-10-08 19:21:24 -0400

    Animals generated

---
+++ Assets/Scripts/AnimalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalManager : MonoBehaviour {
+
+    public GameObject animalPrefab;
+    public static int tankSize = 25;
+
+    static int numAnimal = 20;
+    public static GameObject[] allAnimal = new GameObject[numAnimal];
+	// Use this for initialization
+	void Start () {
+		
+        for(int i = 0; i < numAnimal; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize), 0, Random.Range(-tankSize, tankSize));
+            allAnimal[i] = (GameObject)Instantiate(animalPrefab, pos, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
